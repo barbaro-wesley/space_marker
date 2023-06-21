f1c8cec279e0344fedaeb4b285a8b9bc576edd85
bárbaro-wesley <wesleybarbaro09@gmail.com>
2023-06-21 12:48:39 -0300

    aplicamos a imagem de fundo

---
+++ trabalho.py
@@ -0,0 +1,22 @@
+import pygame
+import random
+import winsound
+
+pygame.init()
+
+tamanho = (800, 600)
+tela = pygame.display.set_mode(tamanho)
+pygame.display.set_caption("space marker")
+clock = pygame.time.Clock()
+fundo = pygame.image.load("bg.jpg")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+           
+
+    tela.blit(fundo, (0, 0))
+    pygame.display.flip()
+    clock.tick(60)
+
