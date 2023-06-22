7be603a888000cf1c7272e714e1040a004a4127f
bárbaro-wesley <wesleybarbaro09@gmail.com>
2023-06-21 21:43:22 -0300

    colocamos o icone e o texto

--- trabalho.py
+++ trabalho.py
@@ -7,9 +7,10 @@ pygame.init()
 tamanho = (800, 600)
 tela = pygame.display.set_mode(tamanho)
 pygame.display.set_caption("space marker")
+icone=pygame.image.load("space_icone.jpg")
+pygame.display.set_icon(icone)
 clock = pygame.time.Clock()
 fundo = pygame.image.load("bg.jpg")
-
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
